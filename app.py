513ff8a5e18d29ed888665ff2587f6c31e3c3d42
srimad1111 <srimadsnehashis@gmail.com>
2025-09-29 11:35:38 +0530

    added fetch poster , recommend fn and pickle module

---
+++ app.py
@@ -0,0 +1,36 @@
+import pickle
+import streamlit as st
+import requests
+
+
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    try:
+        response = requests.get(url, timeout=10, verify=False)
+        response.raise_for_status()
+        data = response.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            return f"https://image.tmdb.org/t/p/w500/{poster_path}"
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+    except Exception as e:
+        print("Error fetching poster:", e)
+        return "https://via.placeholder.com/500x750?text=Error"
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+
